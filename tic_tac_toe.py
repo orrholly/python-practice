e13880276186e1fe97cdf46606049f0b397674f8
Holly Orr <hollyorr@Hollys-MacBook-Pro.local>
2017-10-24 16:43:14 -0400

    code working before adding winner logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -37,32 +37,31 @@ def display_game_board():
     print "\n"
 
 def player1_move():
-    player1_input = check_number()
     while True:
+        player1_input = check_number("Player 1")
         if gameboard[player1_input] != 'x' and gameboard[player1_input] != 'o':
             gameboard[player1_input] = 'x'
-            display_game_board()
             break;
         else:
-            print "That spot is already taken! Try again!"
-            display_game_board()
+            print "That spot is already taken!" + "\n"
+    display_game_board()
 
 def player2_move():
-    player2_input = check_number()
     while True:
-        if player2_input != 'x' and player2_input != 'o':
-            player2_input == 'o'
-            display_game_board()
+        player2_input = check_number("Player 2")
+        if gameboard[player2_input] != 'x' and gameboard[player2_input] != 'o':
+            gameboard[player2_input] = 'o'
             break;
         else:
-            print "That spot is already taken! Try again!"
-            display_game_board()
+            print "That spot is already taken!" + "\n"
+    display_game_board()
 
-def check_number():
+
+def check_number(player):
     passed = False
     while passed is False:
-        entered_number = raw_input("Select a spot by it's number on the board:  ")
-        int_done = convert_to_number(entered_number)
+        entered_player = raw_input(player + " It's your turn. Enter the number of the spot on the board:  " + "\n")
+        int_done = convert_to_number(entered_player)
         passed = test_range(int_done)
     return int_done
 
@@ -75,13 +74,13 @@ def convert_to_number(validate_int):
             int_converted = int(int_converted)
             result = int_converted
         except ValueError:
-            int_converted = raw_input("You have not entered a numeric value. Try again:  " )
+            int_converted = raw_input("You have not entered a numeric value. Try again:  " + "\n")
     return int_converted
 
 
 def test_range(user_int):
-    if user_int not in gameboard:
-        print "You have not entered a spot by it number between 1 - 9. "
+    if user_int not in r:
+        print "You have not entered a spot by it number between 0 - 8. "
         return False
     else:
         return True
@@ -95,6 +94,8 @@ def test_range(user_int):
 # r = range(9)
 # gameboard = [(i + 1) for i in r]
 gameboard = range(9)
+# for error checking that the chosen spot is in range
+r = range(9)
 
 
 # **************************************************************
